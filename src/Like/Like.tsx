754e0f764a0b32add2b5c4b2c22f8b21b3c8a137
RAHEL <richassefa27@gmail.com>
2024-03-06 17:37:56 -0800

    console

--- src/Like/Like.tsx
+++ src/Like/Like.tsx
@@ -8,7 +8,7 @@ const Like = () => {
     const [liked, setLiked] = useState(true);
     const  Click = () => {
         
-        console.log( !liked ? 'unlike': 'like');
+        console.log( !liked ? 'liked': 'unliked');
 
         setLiked(!liked);
     }
@@ -21,7 +21,9 @@ const Like = () => {
                 <img src="https://i.guim.co.uk/img/media/26392d05302e02f7bf4eb143bb84c8097d09144b/446_167_3683_2210/master/3683.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=11e949fc5d06576bc8b80ec192896753" alt="" />
             </div>
             <div className="heart" onClick={ Click}>
-             {liked ? <FaHeart size = {30}/>: <FaRegHeart  /> }
+             {liked ? <FaHeart size = {30}/> : <FaRegHeart  /> }
+            
+             
                 </div>
 
               
